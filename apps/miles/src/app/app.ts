bcfd1ad88e935543543b73084ad4bbdf41579ffd
Michael Egger-Zikes <23002661+mikezks@users.noreply.github.com>
2026-03-23 15:52:40 +0100

    build: miles app

---
+++ apps/miles/src/app/app.ts
@@ -0,0 +1,12 @@
+import { Component, signal } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.html',
+  styleUrl: './app.scss'
+})
+export class App {
+  protected readonly title = signal('miles');
+}
+
+export default App;
